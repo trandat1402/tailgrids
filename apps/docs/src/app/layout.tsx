c7af4c07430fc92e39bb5e9d9c6886dfe80e02d3
trandat <trandat142@gmail.com>
2026-08-12 00:27:48 +0700

    fix(docs): provide local metadata URL fallback

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -26,7 +26,7 @@ export const viewport = {
 };
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"),
   title: {
     template: "%s - Tailgrids UI",
     default: "Tailgrids UI"
